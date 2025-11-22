414628400def066128d0f1031f855bad92f42294
CYC <cheng246355@gmail.com>
2025-11-23 00:51:35 +0800

    feat: Implement product image hover effect on shop page and refine quiz page styling.

--- shop.js
+++ shop.js
@@ -4,8 +4,8 @@
  * @namespace ShopProductGrid
  * @description Logic for dynamically loading product cards in the shop page with infinite scrolling.
  */
-(function() {
-    document.addEventListener('DOMContentLoaded', function() {
+(function () {
+    document.addEventListener('DOMContentLoaded', function () {
         const productGrid = document.getElementById('product-grid');
         const template = document.getElementById('product-card-template');
 
@@ -18,132 +18,154 @@
                 name: '10% Niacinamide + HA',
                 price: 'RM 62.00',
                 imageUrl: 'images/Products/10HA_04-1200x1600.jpg',
+                hoverImageUrl: 'images/Products/10HA_02-1200x1600.jpg',
                 link: 'product.html'
             },
             {
                 name: '100% Squalane Oil',
                 price: 'RM 68.00',
                 imageUrl: 'images/Products/100SqualaneOil_01-1200x1600.jpg',
+                hoverImageUrl: 'images/Products/100SqualaneOil_03-1200x1600.jpg',
                 link: 'product.html'
             },
             {
                 name: '10% Mandelic Acid + PHA',
                 price: 'RM 68.00',
                 imageUrl: 'images/Products/10Mandelic Acid01-1200x1600.jpg',
+                hoverImageUrl: 'images/Products/10Mandelic Acid02-1200x1600.jpg',
                 link: 'product.html'
             },
             {
                 name: '18% Ethyl Ascorbic Acid',
                 price: 'RM 88.00',
                 imageUrl: 'images/Products/18EAA_01-1200x1600.jpg',
+                hoverImageUrl: 'images/Products/18EAA_03-1200x1600.jpg',
                 link: 'product.html'
             },
             {
                 name: '2% Salicylic Acid + Tea Tree Oil',
                 price: 'RM 58.00',
                 imageUrl: 'images/Products/2BHA_01-1200x1600.jpg',
+                hoverImageUrl: 'images/Products/2BHA_03-1200x1600.jpg',
                 link: 'product.html'
             },
             {
                 name: '2% Alpha Arbutin + 3% Tranexamic Acid',
                 price: 'RM 68.00',
                 imageUrl: 'images/Products/2AA_01-1200x1600.jpg',
+                hoverImageUrl: 'images/Products/2AA_03-1200x1600.jpg',
                 link: 'product.html'
             },
             {
                 name: '5% Niacinamide + 2% Alpha Arbutin Toner',
                 price: 'RM 68.00',
                 imageUrl: 'images/Products/AAToner_03-1200x1600.jpg',
+                hoverImageUrl: 'images/Products/AAToner_05-1200x1600.jpg',
                 link: 'product.html'
             },
             {
                 name: '6% Hyaluronic Acid + 2% Vitamin B5',
                 price: 'RM 68.00',
                 imageUrl: 'images/Products/6HA_01-1200x1600.jpg',
+                hoverImageUrl: 'images/Products/6HA_03-1200x1600.jpg',
                 link: 'product.html'
             },
             {
                 name: 'Ceramic Skin Saviour Moisturizer',
                 price: 'RM 88.00',
                 imageUrl: 'images/Products/CeramicCream_01-1200x1600.jpg',
+                hoverImageUrl: 'images/Products/CeramicCream_03-1200x1600.jpg',
                 link: 'product.html'
             },
             {
                 name: 'Gentle Fresh Cleansing Gel',
                 price: 'RM 50.00',
                 imageUrl: 'images/Products/CleansingGel_01-1200x1600.jpg',
+                hoverImageUrl: 'images/Products/CleansingGel_03-1200x1600.jpg',
                 link: 'product.html'
             },
             {
                 name: 'Niacinamide Hydra Gel Moisturizer',
                 price: 'RM 78.00',
                 imageUrl: 'images/Products/NiaGelCream_01-1200x1600.jpg',
+                hoverImageUrl: 'images/Products/NiaGelCream_03-1200x1600.jpg',
                 link: 'product.html'
             },
             {
                 name: 'Niacinamide Polypeptide Amino-arbutin Essence',
                 price: 'RM 78.00',
                 imageUrl: 'images/Products/NPAA_01-1200x1600.jpg',
+                hoverImageUrl: 'images/Products/NPAA_03-1200x1600.jpg',
                 link: 'product.html'
             },
             {
                 name: 'Oat Panthenol Probiotic Essence',
                 price: 'RM 78.00',
                 imageUrl: 'images/Products/OatPanthenol_03-1200x1600.jpg',
+                hoverImageUrl: 'images/Products/OatPanthenol_05-1200x1600.jpg',
                 link: 'product.html'
             },
             {
                 name: 'PHA Clarifying Liquid Exfoliant',
                 price: 'RM 68.00',
                 imageUrl: 'images/Products/PHALiquid_03-1200x1600.jpg',
+                hoverImageUrl: 'images/Products/PHALiquid_05-1200x1600.jpg',
                 link: 'product.html'
             },
             {
                 name: 'Retinal Beginner',
                 price: 'RM 78.00',
                 imageUrl: 'images/Products/Crystal Beginner_01-1200x1600.jpg',
+                hoverImageUrl: 'images/Products/Crystal Beginner_03-1200x1600.jpg',
                 link: 'product.html'
             },
             {
                 name: 'Retinal Expert',
                 price: 'RM 88.00',
                 imageUrl: 'images/Products/Crystal Expert_01-1200x1600.jpg',
+                hoverImageUrl: 'images/Products/Crystal Expert_03-1200x1600.jpg',
                 link: 'product.html'
             },
             {
                 name: 'Retinal Pro',
                 price: 'RM 98.00',
                 imageUrl: 'images/Products/Crystal Pro_01-1200x1600.jpg',
+                hoverImageUrl: 'images/Products/Crystal Pro_03-1200x1600.jpg',
                 link: 'product.html'
             },
             {
                 name: 'Multi-peptide Dark Circle + Eye Bag Serum',
                 price: 'RM 78.00',
                 imageUrl: 'images/Products/DarkCircle_01-1200x1600.jpg',
+                hoverImageUrl: 'images/Products/DarkCircle_03-1200x1600.jpg',
                 link: 'product.html'
             },
             {
                 name: 'Turn On The Light! Body Moisturiser',
                 price: 'RM 58.00',
                 imageUrl: 'images/Products/Turn_01-1200x1600.jpg',
+                hoverImageUrl: 'images/Products/Turn_03-1200x1600.jpg',
                 link: 'product.html'
             },
             {
                 name: 'Ruruberry Tote Bag',
                 price: 'RM 35.00',
                 imageUrl: 'images/Products/ToteBag-1200x1600.jpg',
+                hoverImageUrl: 'images/Products/ToteBag-1200x1600.jpg',
                 link: 'product.html'
             },
             {
                 name: 'Ruruberry Tumbler',
                 price: 'RM 45.00',
                 imageUrl: 'images/Products/Tumbler-1200x1600.jpg',
+                hoverImageUrl: 'images/Products/Tumbler-1200x1600.jpg',
                 link: 'product.html'
             },
             {
                 name: 'Pump for Cleansing Gel',
                 price: 'RM 5.00',
                 imageUrl: 'images/Products/Pump-1200x1600.jpg',
+                hoverImageUrl: 'images/Products/Pump6-1200x1600.jpg',
                 link: 'product.html'
             },
         ];
@@ -162,15 +184,24 @@
 
             productsToLoad.forEach(product => {
                 const clone = document.importNode(template.content, true);
-                
+
                 const link = clone.querySelector('.product-card-link');
-                const image = clone.querySelector('.product-card-image');
+                const mainImage = clone.querySelector('.main-image');
+                const hoverImage = clone.querySelector('.hover-image');
                 const name = clone.querySelector('.product-name');
                 const price = clone.querySelector('.product-price');
 
                 link.href = product.link;
-                image.src = product.imageUrl;
-                image.alt = product.name;
+                mainImage.src = product.imageUrl;
+                mainImage.alt = product.name;
+
+                if (product.hoverImageUrl) {
+                    hoverImage.src = product.hoverImageUrl;
+                } else {
+                    hoverImage.src = product.imageUrl;
+                }
+                hoverImage.alt = product.name;
+
                 name.textContent = product.name;
                 price.textContent = product.price;
 
