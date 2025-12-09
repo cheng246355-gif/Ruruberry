2b7ebebb4b8da2c940b7619900ea7b3fe0db79e3
CYC <cheng246355@gmail.com>
2025-12-10 02:24:07 +0800

    create product pages for each product

--- js/products.js
+++ js/products.js
@@ -1,178 +1,186 @@
 export const allProducts = [
     {
-        name: '10% Niacinamide + HA',
-        price: 'RM 62.00',
-        imageUrl: 'images/Products/10HA_04-1200x1600.jpg',
-        hoverImageUrl: 'images/Products/10HA_02-1200x1600.jpg',
-        link: 'product.html',
-        category: 'booster-drop'
+        "name": "10% Niacinamide + HA",
+        "price": "RM 62.00",
+        "imageUrl": "images/Products/10HA_04-1200x1600.jpg",
+        "hoverImageUrl": "images/Products/10HA_02-1200x1600.jpg",
+        "category": "booster-drop",
+        "link": "products/10-niacinamide-ha.html"
     },
     {
-        name: '100% Squalane Oil',
-        price: 'RM 68.00',
-        imageUrl: 'images/Products/100SqualaneOil_01-1200x1600.jpg',
-        hoverImageUrl: 'images/Products/100SqualaneOil_03-1200x1600.jpg',
-        link: 'product.html',
-        category: 'booster-drop'
+        "name": "100% Squalane Oil",
+        "price": "RM 68.00",
+        "imageUrl": "images/Products/100SqualaneOil_01-1200x1600.jpg",
+        "hoverImageUrl": "images/Products/100SqualaneOil_03-1200x1600.jpg",
+        "category": "booster-drop",
+        "link": "products/100-squalane-oil.html"
     },
     {
-        name: '10% Mandelic Acid + PHA',
-        price: 'RM 68.00',
-        imageUrl: 'images/Products/10Mandelic Acid01-1200x1600.jpg',
-        hoverImageUrl: 'images/Products/10Mandelic Acid02-1200x1600.jpg',
-        link: 'product.html',
-        category: 'essence-toner'
+        "name": "10% Mandelic Acid + PHA",
+        "price": "RM 68.00",
+        "imageUrl": "images/Products/10Mandelic Acid01-1200x1600.jpg",
+        "hoverImageUrl": "images/Products/10Mandelic Acid02-1200x1600.jpg",
+        "category": "essence-toner",
+        "link": "products/10-mandelic-acid-pha.html"
     },
     {
-        name: '18% Ethyl Ascorbic Acid',
-        price: 'RM 88.00',
-        imageUrl: 'images/Products/18EAA_01-1200x1600.jpg',
-        hoverImageUrl: 'images/Products/18EAA_03-1200x1600.jpg',
-        link: 'product.html',
-        category: 'booster-drop'
+        "name": "18% Ethyl Ascorbic Acid",
+        "price": "RM 88.00",
+        "imageUrl": "images/Products/18EAA_01-1200x1600.jpg",
+        "hoverImageUrl": "images/Products/18EAA_03-1200x1600.jpg",
+        "category": "booster-drop",
+        "link": "products/18-ethyl-ascorbic-acid.html"
     },
     {
-        name: '2% Salicylic Acid + Tea Tree Oil',
-        price: 'RM 58.00',
-        imageUrl: 'images/Products/2BHA_01-1200x1600.jpg',
-        hoverImageUrl: 'images/Products/2BHA_03-1200x1600.jpg',
-        link: 'product.html',
-        category: 'booster-drop'
+        "name": "2% Salicylic Acid + Tea Tree Oil",
+        "price": "RM 58.00",
+        "imageUrl": "images/Products/2BHA_01-1200x1600.jpg",
+        "hoverImageUrl": "images/Products/2BHA_03-1200x1600.jpg",
+        "category": "booster-drop",
+        "link": "products/2-salicylic-acid-tea-tree-oil.html"
     },
     {
-        name: '2% Alpha Arbutin + 3% Tranexamic Acid',
-        price: 'RM 68.00',
-        imageUrl: 'images/Products/2AA_01-1200x1600.jpg',
-        hoverImageUrl: 'images/Products/2AA_03-1200x1600.jpg',
-        link: 'product.html',
-        category: 'booster-drop'
+        "name": "2% Alpha Arbutin + 3% Tranexamic Acid",
+        "price": "RM 68.00",
+        "imageUrl": "images/Products/2AA_01-1200x1600.jpg",
+        "hoverImageUrl": "images/Products/2AA_03-1200x1600.jpg",
+        "category": "booster-drop",
+        "link": "products/2-alpha-arbutin-3-tranexamic-acid.html"
     },
     {
-        name: '5% Niacinamide + 2% Alpha Arbutin Toner',
-        price: 'RM 68.00',
-        imageUrl: 'images/Products/AAToner_03-1200x1600.jpg',
-        hoverImageUrl: 'images/Products/AAToner_05-1200x1600.jpg',
-        link: 'product.html',
-        category: 'essence-toner'
+        "name": "5% Niacinamide + 2% Alpha Arbutin Toner",
+        "price": "RM 68.00",
+        "imageUrl": "images/Products/AAToner_03-1200x1600.jpg",
+        "hoverImageUrl": "images/Products/AAToner_05-1200x1600.jpg",
+        "category": "essence-toner",
+        "link": "products/5-niacinamide-2-alpha-arbutin-toner.html"
     },
     {
-        name: '6% Hyaluronic Acid + 2% Vitamin B5',
-        price: 'RM 68.00',
-        imageUrl: 'images/Products/6HA_01-1200x1600.jpg',
-        hoverImageUrl: 'images/Products/6HA_03-1200x1600.jpg',
-        link: 'product.html',
-        category: 'booster-drop'
+        "name": "6% Hyaluronic Acid + 2% Vitamin B5",
+        "price": "RM 68.00",
+        "imageUrl": "images/Products/6HA_01-1200x1600.jpg",
+        "hoverImageUrl": "images/Products/6HA_03-1200x1600.jpg",
+        "category": "booster-drop",
+        "link": "products/6-hyaluronic-acid-2-vitamin-b5.html"
     },
     {
-        name: 'Ceramic Skin Saviour Moisturizer',
-        price: 'RM 88.00',
-        imageUrl: 'images/Products/CeramicCream_01-1200x1600.jpg',
-        hoverImageUrl: 'images/Products/CeramicCream_03-1200x1600.jpg',
-        link: 'product.html',
-        category: 'moisturizer'
+        "name": "Ceramic Skin Saviour Moisturizer",
+        "price": "RM 88.00",
+        "imageUrl": "images/Products/CeramicCream_01-1200x1600.jpg",
+        "hoverImageUrl": "images/Products/CeramicCream_03-1200x1600.jpg",
+        "category": "moisturizer",
+        "link": "products/ceramic-skin-saviour-moisturizer.html"
     },
     {
-        name: 'Gentle Fresh Cleansing Gel',
-        price: 'RM 50.00',
-        imageUrl: 'images/Products/CleansingGel_01-1200x1600.jpg',
-        hoverImageUrl: 'images/Products/CleansingGel_03-1200x1600.jpg',
-        link: 'product.html',
-        category: 'cleanser'
+        "name": "Gentle Fresh Cleansing Gel",
+        "price": "RM 50.00",
+        "imageUrl": "images/Products/CleansingGel_01-1200x1600.jpg",
+        "hoverImageUrl": "images/Products/CleansingGel_03-1200x1600.jpg",
+        "category": "cleanser",
+        "link": "products/gentle-fresh-cleansing-gel.html"
     },
     {
-        name: 'Niacinamide Hydra Gel Moisturizer',
-        price: 'RM 78.00',
-        imageUrl: 'images/Products/NiaGelCream_01-1200x1600.jpg',
-        hoverImageUrl: 'images/Products/NiaGelCream_03-1200x1600.jpg',
-        link: 'product.html',
-        category: 'moisturizer'
+        "name": "Niacinamide 10% + Zinc Serum",
+        "price": "RM 45.00",
+        "imageUrl": "images/Products/10Zinc30ml_06-1200x1600.jpg",
+        "hoverImageUrl": "images/Products/10Zinc30ml_02-1200x1600.jpg",
+        "category": "booster-drop",
+        "link": "products/10-niacinamide-zinc-serum.html"
     },
     {
-        name: 'Niacinamide Polypeptide Amino-arbutin Essence',
-        price: 'RM 78.00',
-        imageUrl: 'images/Products/NPAA_01-1200x1600.jpg',
-        hoverImageUrl: 'images/Products/NPAA_03-1200x1600.jpg',
-        link: 'product.html',
-        category: 'essence-toner'
+        "name": "Niacinamide Hydra Gel Moisturizer",
+        "price": "RM 78.00",
+        "imageUrl": "images/Products/NiaGelCream_01-1200x1600.jpg",
+        "hoverImageUrl": "images/Products/NiaGelCream_03-1200x1600.jpg",
+        "category": "moisturizer",
+        "link": "products/niacinamide-hydra-gel-moisturizer.html"
     },
     {
-        name: 'Oat Panthenol Probiotic Essence',
-        price: 'RM 78.00',
-        imageUrl: 'images/Products/OatPanthenol_03-1200x1600.jpg',
-        hoverImageUrl: 'images/Products/OatPanthenol_05-1200x1600.jpg',
-        link: 'product.html',
-        category: 'essence-toner'
+        "name": "Niacinamide Polypeptide Amino-arbutin Essence",
+        "price": "RM 78.00",
+        "imageUrl": "images/Products/NPAA_01-1200x1600.jpg",
+        "hoverImageUrl": "images/Products/NPAA_03-1200x1600.jpg",
+        "category": "essence-toner",
+        "link": "products/niacinamide-polypeptide-amino-arbutin-essence.html"
     },
     {
-        name: 'PHA Clarifying Liquid Exfoliant',
-        price: 'RM 68.00',
-        imageUrl: 'images/Products/PHALiquid_03-1200x1600.jpg',
-        hoverImageUrl: 'images/Products/PHALiquid_05-1200x1600.jpg',
-        link: 'product.html',
-        category: 'essence-toner'
+        "name": "Oat Panthenol Probiotic Essence",
+        "price": "RM 78.00",
+        "imageUrl": "images/Products/OatPanthenol_03-1200x1600.jpg",
+        "hoverImageUrl": "images/Products/OatPanthenol_05-1200x1600.jpg",
+        "category": "essence-toner",
+        "link": "products/oat-panthenol-probiotic-essence.html"
     },
     {
-        name: 'Retinal Beginner',
-        price: 'RM 78.00',
-        imageUrl: 'images/Products/Crystal Beginner_01-1200x1600.jpg',
-        hoverImageUrl: 'images/Products/Crystal Beginner_03-1200x1600.jpg',
-        link: 'product.html',
-        category: 'booster-drop'
+        "name": "PHA Clarifying Liquid Exfoliant",
+        "price": "RM 68.00",
+        "imageUrl": "images/Products/PHALiquid_03-1200x1600.jpg",
+        "hoverImageUrl": "images/Products/PHALiquid_05-1200x1600.jpg",
+        "category": "essence-toner",
+        "link": "products/pha-clarifying-liquid-exfoliant.html"
     },
     {
-        name: 'Retinal Expert',
-        price: 'RM 88.00',
-        imageUrl: 'images/Products/Crystal Expert_01-1200x1600.jpg',
-        hoverImageUrl: 'images/Products/Crystal Expert_03-1200x1600.jpg',
-        link: 'product.html',
-        category: 'booster-drop'
+        "name": "Retinal Beginner",
+        "price": "RM 78.00",
+        "imageUrl": "images/Products/Crystal Beginner_01-1200x1600.jpg",
+        "hoverImageUrl": "images/Products/Crystal Beginner_03-1200x1600.jpg",
+        "category": "booster-drop",
+        "link": "products/retinal-beginner.html"
     },
     {
-        name: 'Retinal Pro',
-        price: 'RM 98.00',
-        imageUrl: 'images/Products/Crystal Pro_01-1200x1600.jpg',
-        hoverImageUrl: 'images/Products/Crystal Pro_03-1200x1600.jpg',
-        link: 'product.html',
-        category: 'booster-drop'
+        "name": "Retinal Expert",
+        "price": "RM 88.00",
+        "imageUrl": "images/Products/Crystal Expert_01-1200x1600.jpg",
+        "hoverImageUrl": "images/Products/Crystal Expert_03-1200x1600.jpg",
+        "category": "booster-drop",
+        "link": "products/retinal-expert.html"
     },
     {
-        name: 'Multi-peptide Dark Circle + Eye Bag Serum',
-        price: 'RM 78.00',
-        imageUrl: 'images/Products/DarkCircle_01-1200x1600.jpg',
-        hoverImageUrl: 'images/Products/DarkCircle_03-1200x1600.jpg',
-        link: 'product.html',
-        category: 'booster-drop'
+        "name": "Retinal Pro",
+        "price": "RM 98.00",
+        "imageUrl": "images/Products/Crystal Pro_01-1200x1600.jpg",
+        "hoverImageUrl": "images/Products/Crystal Pro_03-1200x1600.jpg",
+        "category": "booster-drop",
+        "link": "products/retinal-pro.html"
     },
     {
-        name: 'Turn On The Light! Body Moisturiser',
-        price: 'RM 58.00',
-        imageUrl: 'images/Products/Turn_01-1200x1600.jpg',
-        hoverImageUrl: 'images/Products/Turn_03-1200x1600.jpg',
-        link: 'product.html',
-        category: 'moisturizer'
+        "name": "Multi-peptide Dark Circle + Eye Bag Serum",
+        "price": "RM 78.00",
+        "imageUrl": "images/Products/DarkCircle_01-1200x1600.jpg",
+        "hoverImageUrl": "images/Products/DarkCircle_03-1200x1600.jpg",
+        "category": "booster-drop",
+        "link": "products/multi-peptide-dark-circle-eye-bag-serum.html"
     },
     {
-        name: 'Ruruberry Tote Bag',
-        price: 'RM 35.00',
-        imageUrl: 'images/Products/ToteBag-1200x1600.jpg',
-        hoverImageUrl: 'images/Products/ToteBag-1200x1600.jpg',
-        link: 'product.html',
-        category: 'lifestyle-collection'
+        "name": "Turn On The Light! Body Moisturiser",
+        "price": "RM 58.00",
+        "imageUrl": "images/Products/Turn_01-1200x1600.jpg",
+        "hoverImageUrl": "images/Products/Turn_03-1200x1600.jpg",
+        "category": "moisturizer",
+        "link": "products/turn-on-the-light-body-moisturiser.html"
     },
     {
-        name: 'Ruruberry Tumbler',
-        price: 'RM 45.00',
-        imageUrl: 'images/Products/Tumbler-1200x1600.jpg',
-        hoverImageUrl: 'images/Products/Tumbler-1200x1600.jpg',
-        link: 'product.html',
-        category: 'lifestyle-collection'
+        "name": "Ruruberry Tote Bag",
+        "price": "RM 35.00",
+        "imageUrl": "images/Products/ToteBag-1200x1600.jpg",
+        "hoverImageUrl": "images/Products/ToteBag-1200x1600.jpg",
+        "category": "lifestyle-collection",
+        "link": "products/ruruberry-tote-bag.html"
     },
     {
-        name: 'Pump for Cleansing Gel',
-        price: 'RM 5.00',
-        imageUrl: 'images/Products/Pump-1200x1600.jpg',
-        hoverImageUrl: 'images/Products/Pump6-1200x1600.jpg',
-        link: 'product.html',
-        category: 'lifestyle-collection'
+        "name": "Ruruberry Tumbler",
+        "price": "RM 45.00",
+        "imageUrl": "images/Products/Tumbler-1200x1600.jpg",
+        "hoverImageUrl": "images/Products/Tumbler-1200x1600.jpg",
+        "category": "lifestyle-collection",
+        "link": "products/ruruberry-tumbler.html"
     },
+    {
+        "name": "Pump for Cleansing Gel",
+        "price": "RM 5.00",
+        "imageUrl": "images/Products/Pump-1200x1600.jpg",
+        "hoverImageUrl": "images/Products/Pump6-1200x1600.jpg",
+        "category": "lifestyle-collection",
+        "link": "products/pump-for-cleansing-gel.html"
+    }
 ];
